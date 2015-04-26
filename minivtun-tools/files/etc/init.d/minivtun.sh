113dd5b712d7700c6c87d86df35b7213cd05ec99
rssnsj <rssn@163.com>
2015-04-26 22:50:48 +0800

    minivtun-tools/: Modified for Ubuntu/CentOS.

--- minivtun-tools/files/etc/init.d/minivtun.sh
+++ minivtun-tools/files/etc/init.d/minivtun.sh
@@ -1,13 +1,29 @@
-#!/bin/sh /etc/rc.common
+#!/bin/sh
 #
 # Copyright (C) 2014 Justin Liu <rssnsj@gmail.com>
-# https://github.com/rssnsj/network-feeds
 #
+
+### BEGIN INIT INFO
+# Provides: minivtun-tools
+# Required-Start:
+# Required-Stop:
+# Default-Start: 2 3 4 5
+# Default-Stop:
+# Short-Description: Non-standard VPN that helps you to get through firewalls
+### END INIT INFO
 
 START=93
 
 VPN_ROUTE_FWMARK=199
 VPN_IPROUTE_TABLE=virtual
+DNSMASQ_PORT=53
+DNSMASQ_PIDFILE=/var/run/dnsmasq-go.pid
+
+[ -f /etc/default/minivtun ] && . /etc/default/minivtun
+if [ -z "$vt_enabled" -o "$vt_enabled" = 0 ]; then
+	echo "WARNING: Mini Virtual Tunneller is disabled."
+	return 1
+fi
 
 __netmask_to_bits()
 {
@@ -37,29 +53,23 @@ __netmask_to_bits()
 # New implementation:
 # Attach rules to main 'dnsmasq' service and restart it.
 
-start()
+do_start()
 {
-	local vt_enabled=`uci get minivtun.@minivtun[0].enabled 2>/dev/null`
-	local vt_network=`uci get minivtun.@minivtun[0].network 2>/dev/null`
-	local vt_server_addr=`uci get minivtun.@minivtun[0].server`
-	local vt_server_port=`uci get minivtun.@minivtun[0].server_port`
-	local vt_password=`uci get minivtun.@minivtun[0].password 2>/dev/null`
-	local vt_local_ipaddr=`uci get minivtun.@minivtun[0].local_ipaddr 2>/dev/null`
-	local vt_local_netmask=`uci get minivtun.@minivtun[0].local_netmask 2>/dev/null`
-	local vt_local_ip6pair=`uci get minivtun.@minivtun[0].local_ip6pair 2>/dev/null`
-	local vt_safe_dns=`uci get minivtun.@minivtun[0].safe_dns 2>/dev/null`
-	local vt_safe_dns_port=`uci get minivtun.@minivtun[0].safe_dns_port 2>/dev/null`
-	local vt_proxy_mode=`uci get minivtun.@minivtun[0].proxy_mode`
-	#local vt_protocols=`uci get minivtun.@minivtun[0].protocols 2>/dev/null`
-	# $covered_subnets, $local_addresses are not required
-	local covered_subnets=`uci get minivtun.@minivtun[0].covered_subnets 2>/dev/null`
-	local local_addresses=`uci get minivtun.@minivtun[0].local_addresses 2>/dev/null`
-
-	# -----------------------------------------------------------------
-	if [ "$vt_enabled" = 0 ]; then
-		echo "WARNING: Mini Virtual Tunneller is disabled."
-		return 1
-	fi
+#	local vt_enabled=`uci get minivtun.@minivtun[0].enabled 2>/dev/null`
+#	local vt_network=`uci get minivtun.@minivtun[0].network 2>/dev/null`
+#	local vt_server_addr=`uci get minivtun.@minivtun[0].server`
+#	local vt_server_port=`uci get minivtun.@minivtun[0].server_port`
+#	local vt_password=`uci get minivtun.@minivtun[0].password 2>/dev/null`
+#	local vt_local_ipaddr=`uci get minivtun.@minivtun[0].local_ipaddr 2>/dev/null`
+#	local vt_local_netmask=`uci get minivtun.@minivtun[0].local_netmask 2>/dev/null`
+#	local vt_local_ip6pair=`uci get minivtun.@minivtun[0].local_ip6pair 2>/dev/null`
+#	local vt_safe_dns=`uci get minivtun.@minivtun[0].safe_dns 2>/dev/null`
+#	local vt_safe_dns_port=`uci get minivtun.@minivtun[0].safe_dns_port 2>/dev/null`
+#	local vt_proxy_mode=`uci get minivtun.@minivtun[0].proxy_mode`
+#	#local vt_protocols=`uci get minivtun.@minivtun[0].protocols 2>/dev/null`
+#	# $covered_subnets, $local_addresses are not required
+#	local covered_subnets=`uci get minivtun.@minivtun[0].covered_subnets 2>/dev/null`
+#	local local_addresses=`uci get minivtun.@minivtun[0].local_addresses 2>/dev/null`
 
 	if [ -z "$vt_server_addr" -o -z "$vt_server_port" ]; then
 		echo "WARNING: No server address configured, not starting."
@@ -70,9 +80,8 @@ start()
 	[ -z "$vt_proxy_mode" ] && vt_proxy_mode=S
 	[ -z "$vt_safe_dns_port" ] && vt_safe_dns_port=53
 	# Get LAN settings as default parameters
-	[ -f /lib/functions/network.sh ] && . /lib/functions/network.sh
-	[ -z "$covered_subnets" ] && network_get_subnet covered_subnets lan
-	[ -z "$local_addresses" ] && network_get_ipaddr local_addresses lan
+	[ -z "$covered_subnets" ] && covered_subnets="192.168.10.0/24"
+	[ -z "$local_addresses" ] && local_addresses="192.168.10.1"
 	local vt_ifname="minivtun-$vt_network"
 	local vt_local_prefix=`__netmask_to_bits "$vt_local_netmask"`
 	local vt_gfwlist="china-banned"
@@ -83,33 +92,10 @@ start()
 		-a $vt_local_ipaddr/$vt_local_prefix -n $vt_ifname -e "$vt_password" \
 		-d -p /var/run/$vt_ifname.pid || return 1
 
-	# Create new interface if not exists
-	local __ifname=`uci get network.$vt_network.ifname 2>/dev/null`
-	if [ "$__ifname" != "$vt_ifname" ]; then
-		uci delete network.$vt_network 2>/dev/null
-		uci set network.$vt_network=interface
-		uci set network.$vt_network.proto=none
-		uci set network.$vt_network.ifname=$vt_ifname
-		uci commit network
-
-		# Attach this interface to firewall zone "wan"
-		local i=0
-		while true; do
-			local __zone=`uci get firewall.@zone[$i].name`
-			[ -z "$__zone" ] && break
-			# Match zone "wan" to modify
-			if [ "$__zone" = wan ]; then
-				local __zone_nets=`uci get firewall.@zone[$i].network`
-				uci delete firewall.@zone[$i].network
-				uci set firewall.@zone[$i].network="$__zone_nets $vt_network"
-				uci commit firewall
-				break
-			fi
-			i=`expr $i + 1`
-		done
-	fi
-
-	ifup $vt_network
+	# IMPORTANT: 'rp_filter=1' will cause returned packets from
+	# virtual interface being dropped, so we have to fix it.
+	echo 0 > /proc/sys/net/ipv4/conf/all/rp_filter
+	echo 0 > /proc/sys/net/ipv4/conf/$vt_ifname/rp_filter
 
 	# -----------------------------------------------------------------
 	###### IPv4 firewall rules and policy routing ######
@@ -153,21 +139,17 @@ start()
 			iptables -t mangle -A minivtun_$vt_network -m set ! --match-set gfwlist dst -j RETURN
 			;;
 	esac
-	local subnet
-	for subnet in $covered_subnets; do
-		iptables -t mangle -A minivtun_$vt_network -s $subnet -j MARK --set-mark $VPN_ROUTE_FWMARK
-	done
-	[ -n "$vt_safe_dns" ] && \
-		iptables -t mangle -A minivtun_$vt_network -d $vt_safe_dns -p udp --dport $vt_safe_dns_port -j MARK --set-mark $VPN_ROUTE_FWMARK
+	iptables -t mangle -A minivtun_$vt_network -j MARK --set-mark $VPN_ROUTE_FWMARK
+
 	iptables -t mangle -I PREROUTING -j minivtun_$vt_network
-	iptables -t mangle -I OUTPUT -p udp --dport 53 -j minivtun_$vt_network  # DNS queries over tunnel
+	iptables -t mangle -I OUTPUT -j minivtun_$vt_network
 
 	# -----------------------------------------------------------------
-	mkdir -p /var/etc/dnsmasq-go.d
+	mkdir -p /tmp/etc/dnsmasq-go.d
 	###### Anti-pollution configuration ######
 	if [ -n "$vt_safe_dns" ]; then
 		awk -vs="$vt_safe_dns#$vt_safe_dns_port" '!/^$/&&!/^#/{printf("server=/%s/%s\n",$0,s)}' \
-			/etc/gfwlist/$vt_gfwlist > /var/etc/dnsmasq-go.d/01-pollution.conf
+			/etc/gfwlist/$vt_gfwlist > /tmp/etc/dnsmasq-go.d/01-pollution.conf
 	else
 		echo "WARNING: Not using secure DNS, DNS resolution might be polluted if you are in China."
 	fi
@@ -176,57 +158,52 @@ start()
 	case "$vt_proxy_mode" in
 		M|V)
 			awk '!/^$/&&!/^#/{printf("ipset=/%s/gfwlist\n",$0)}' \
-				/etc/gfwlist/$vt_gfwlist > /var/etc/dnsmasq-go.d/02-ipset.conf
+				/etc/gfwlist/$vt_gfwlist > /tmp/etc/dnsmasq-go.d/02-ipset.conf
 			;;
 	esac
 
 	# -----------------------------------------------------------------
 	###### Restart main 'dnsmasq' service if needed ######
-	if ls /var/etc/dnsmasq-go.d/* >/dev/null 2>&1; then
-		mkdir -p /tmp/dnsmasq.d
-		cat > /tmp/dnsmasq.d/dnsmasq-go.conf <<EOF
-conf-dir=/var/etc/dnsmasq-go.d
+	if ls /tmp/etc/dnsmasq-go.d/* >/dev/null 2>&1; then
+		cat > /tmp/etc/dnsmasq-go.conf <<EOF
+conf-dir=/tmp/etc/dnsmasq-go.d
+resolv-file=/tmp/etc/resolv.conf.auto
 EOF
-		/etc/init.d/dnsmasq restart
-
-		# Check if DNS service was really started
-		local dnsmasq_ok=N
-		local i
-		for i in 0 1 2 3 4 5 6 7; do
-			sleep 1
-			local dnsmasq_pid=`cat /var/run/dnsmasq.pid 2>/dev/null`
-			if [ -n "$dnsmasq_pid" ]; then
-				if kill -0 "$dnsmasq_pid" 2>/dev/null; then
-					dnsmasq_ok=Y
-					break
-				fi
-			fi
-		done
-		if [ "$dnsmasq_ok" != Y ]; then
-			echo "WARNING: Attached dnsmasq rules will cause the service startup failure. Removed those configurations."
-			rm -f /tmp/dnsmasq.d/dnsmasq-go.conf
-			/etc/init.d/dnsmasq restart
+		if ! grep 'nameserver[ \t]\+127\.0\.0\.1' /etc/resolv.conf >/dev/null; then
+			cat /etc/resolv.conf > /tmp/etc/resolv.conf.auto
+		fi
+		if dnsmasq -C /tmp/etc/dnsmasq-go.conf -p $DNSMASQ_PORT -x $DNSMASQ_PIDFILE; then
+			echo "nameserver 127.0.0.1" > /etc/resolv.conf
+		else
+			echo "*** WARNING: 'dnsmasq' service was not started successfully."
 		fi
 	fi
 
 }
 
-stop()
+do_stop()
 {
-	local vt_network=`uci get minivtun.@minivtun[0].network 2>/dev/null`
+#	local vt_network=`uci get minivtun.@minivtun[0].network 2>/dev/null`
+
 	[ -z "$vt_network" ] && vt_network="vt0"
 	local vt_ifname="minivtun-$vt_network"
 
 	# -----------------------------------------------------------------
-	rm -rf /var/etc/dnsmasq-go.d
-	if [ -f /tmp/dnsmasq.d/dnsmasq-go.conf ]; then
-		rm -f /tmp/dnsmasq.d/dnsmasq-go.conf
-		/etc/init.d/dnsmasq restart
+	# Stop 'dnsmasq' service and delete configuration files
+	if [ -f $DNSMASQ_PIDFILE ]; then
+		kill -9 `cat $DNSMASQ_PIDFILE`
+		rm -f $DNSMASQ_PIDFILE
+	fi
+	rm -rf /tmp/etc/dnsmasq-go.d
+	rm -f /tmp/etc/dnsmasq-go.conf
+	# Restore /etc/resolv.conf
+	if grep 'nameserver[ \t]\+127\.0\.0\.1' /etc/resolv.conf >/dev/null; then
+		[ -f /tmp/etc/resolv.conf.auto ] && cat /tmp/etc/resolv.conf.auto > /etc/resolv.conf
 	fi
 
 	# -----------------------------------------------------------------
 	if iptables -t mangle -F minivtun_$vt_network 2>/dev/null; then
-		while iptables -t mangle -D OUTPUT -p udp --dport 53 -j minivtun_$vt_network 2>/dev/null; do :; done
+		while iptables -t mangle -D OUTPUT -j minivtun_$vt_network 2>/dev/null; do :; done
 		while iptables -t mangle -D PREROUTING -j minivtun_$vt_network 2>/dev/null; do :; done
 		iptables -t mangle -X minivtun_$vt_network 2>/dev/null
 	fi
@@ -239,11 +216,27 @@ stop()
 	# it will be brought down along with the interface.
 	while ip rule del fwmark $VPN_ROUTE_FWMARK table $VPN_IPROUTE_TABLE 2>/dev/null; do :; done
 
-	ifdown $vt_network
 	if [ -f /var/run/$vt_ifname.pid ]; then
 		kill -9 `cat /var/run/$vt_ifname.pid`
 		rm -f /var/run/$vt_ifname.pid
 	fi
 
 }
+
+case "$1" in
+	start)
+		do_start
+		;;
+	stop)
+		do_stop
+		;;
+	restart)
+		do_stop
+		do_start
+		;;
+	*)
+		echo "Usage: $0 {start|stop|restart}"
+		exit 1
+		;;
+esac
 
