6e956a00a060b199f011f354fd594ac89e464654
rssnsj <rssn@163.com>
2015-04-26 21:11:36 +0800

    ipset-lists/: Modified for Ubuntu/CentOS distributions.

--- ipset-lists/files/etc/init.d/ipset.sh
+++ ipset-lists/files/etc/init.d/ipset.sh
@@ -1,9 +1,18 @@
-#!/bin/sh /etc/rc.common
+#!/bin/sh
+#
 # Copyright (C) 2014 Justin Liu <rssnsj@gmail.com>
+#
 
-START=21
+### BEGIN INIT INFO
+# Provides:     ipset-lists
+# Required-Start:
+# Required-Stop:
+# Default-Start:    2 3 4 5
+# Default-Stop:
+# Short-Description:    ipset tables and gfwlist
+### END INIT INFO
 
-start()
+do_start()
 {
 	local file
 	for file in /etc/ipset/*; do
@@ -11,8 +20,25 @@ start()
 	done
 }
 
-stop()
+do_stop()
 {
 	ipset destroy
 }
+
+case "$1" in
+	start)
+		do_start
+		;;
+	stop)
+		do_stop
+		;;
+	restart)
+		do_stop
+		do_start
+		;;
+	*)
+		echo "Usage: $0 {start|stop|restart}"
+		exit 1
+		;;
+esac
 
